bc9334ba7fefdbeb48aa2d53567a41e1d6cc837f
devDalys <vip.viktor00@mail.ru>
2021-09-02 21:13:23 +0300

    add js-validation

---
+++ ABS-Electro/js/main.js
@@ -0,0 +1,32 @@
+const btnForm = document.querySelector('.form_btn');
+const btnPass = document.querySelector('.pass');
+const validation = (array) =>{
+    for(let item of array){
+        if(item.value.trim() == ''){
+            const label = item.nextElementSibling;
+            item.classList.add('inputError')
+            label.classList.add('inputInfo_error');
+            const error = label.nextElementSibling;
+            error.classList.remove('hide')
+        }
+        else {
+            const label = item.nextElementSibling;
+            item.classList.remove('inputError')
+            label.classList.remove('inputInfo_error');
+            const error = label.nextElementSibling;
+            error.classList.add('hide')
+        }
+    }
+}
+
+
+btnForm.addEventListener('click',(event) => {
+    event.preventDefault();
+    const arrayOfData = document.querySelectorAll('.inputOfForm');
+    validation(arrayOfData)
+})
+btnPass.addEventListener('click',(event) => {
+    event.preventDefault();
+    const arrayOfData = document.querySelectorAll('.input');
+    validation(arrayOfData)
+})
