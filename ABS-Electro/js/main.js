ac69ed48abfaffe35596b2a37fa933a097855525
devDalys <vip.viktor00@mail.ru>
2021-09-03 20:31:54 +0300

    core-review

--- ABS-Electro/js/main.js
+++ ABS-Electro/js/main.js
@@ -1,8 +1,8 @@
 const btnForm = document.querySelector('.form_btn');
 const btnPass = document.querySelector('.pass');
-const validation = (array) =>{
-    for(let item of array){
-        if(item.value.trim() == ''){
+const validation = (array) => {
+    for (let item of array) {
+        if (item.value.trim() == '') {
             const label = item.nextElementSibling;
             item.classList.add('inputError')
             label.classList.add('inputInfo_error');
@@ -20,12 +20,12 @@ const validation = (array) =>{
 }
 
 
-btnForm.addEventListener('click',(event) => {
+btnForm.addEventListener('click', (event) => {
     event.preventDefault();
     const arrayOfData = document.querySelectorAll('.inputOfForm');
     validation(arrayOfData)
 })
-btnPass.addEventListener('click',(event) => {
+btnPass.addEventListener('click', (event) => {
     event.preventDefault();
     const arrayOfData = document.querySelectorAll('.input');
     validation(arrayOfData)
