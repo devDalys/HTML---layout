f7872a06a80d8fa09bfb8b62d8e1e9a4cbe13226
devDalys <vip.viktor00@mail.ru>
2021-09-04 14:52:21 +0300

    Fixed bug with ID

--- todolist-react/src/CreateTask.js
+++ todolist-react/src/CreateTask.js
@@ -1,26 +1,17 @@
-import React, { useEffect,  useState } from 'react'
+import React from 'react'
 import './app.css'
 function CreateTask(props){
-    console.log(props.lastChildId)
-    // const getId = (idx) =>{
-    //     let id = 0;
-    //     if (idx){id = idx }else {id = 1}
-    //     console.log(id);
-    //     return id;
-    // }
     const add_btn = (event) =>{
         event.preventDefault();
         let value = document.querySelector('#AddTask__input').value;
         let id = props.lastChildId;
-        let item = {id: id++, text: value, done: false};
+        let item = {id: Date.now(), text: value, done: false};
         if(item.text){props.onFormSubmit(item); props.onCloseForm(false)} else alert('Введите текст задачи')
         document.querySelector('#AddTask__input').value = '';
-        // props.onCloseForm(false);
     }
     const closeBtn = (event) => {
         props.onCloseForm(false)
     }
-
 return(
     <div className = "Addtask">
         <form className= 'inputForm'>
