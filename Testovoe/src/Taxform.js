e15d813047c4a9fbe90fcec667ed3a2451e95c0b
Victor <86551854+devDalys@users.noreply.github.com>
2021-12-02 09:22:20 +0300

    create main panel

---
+++ Testovoe/src/Taxform.js
@@ -0,0 +1,29 @@
+import React from "react";
+import './App.css'
+
+function declOfNum(n, titles) {
+    console.log(n);
+    return titles[n % 10 == 1 && n % 100 != 11 ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+}
+
+function Taxform(props) {
+    console.log(props.massive)
+    return (
+        <div className='list__items'>
+            {props.massive.map((item, index) => {
+                while (index < 100) {
+                    return (
+                        <div className='list__items-item'>
+                            <input type="checkbox"/>
+                            <p>{Math.round(item)} {declOfNum(Math.round(item), ['рубль', 'рубля', 'рублей'])} <span
+                                className='grey__text'>в {index + 1}-й год </span></p>
+                        </div>
+                    )
+                }
+
+            })}
+        </div>
+    )
+}
+
+export default Taxform;
